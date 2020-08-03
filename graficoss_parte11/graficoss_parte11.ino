e7f3bae5a15839e8eae0349c68b77487a2adf021
Guilherme Hugentobler <guilherme.hugentobler@benkyou.com.br>
2020-08-03 20:56:49 -0300

    (Trabalhando com Gráficos

---
+++ graficoss_parte11/graficoss_parte11.ino
@@ -0,0 +1,24 @@
+const int pinoPotenciometro = A0; // pino onde o potenciometro está conectado
+int leitura = 0; // variável para armazenar o valor lido pelo ADC
+float tensao = 0.0; // variável para armazenar a tensão
+
+void setup() {
+  // Inicia e configura a Serial
+  Serial.begin(9600); // 9600 bps
+
+  // configura o pino com potenciometro como entrada
+  pinMode(pinoPotenciometro, INPUT); // pino A0
+}
+
+void loop() {
+  // le o valor de tensão no pino do potenciometro
+  leitura = analogRead(pinoPotenciometro);
+
+  // converte e imprime o valor em tensão elétrica
+  tensao = leitura * 5.0 / 1024.0;
+
+  // imprime valor no plotter serial
+  Serial.println(tensao);
+  
+  delay(100); // aguarda 100 milissegundos para uma nova leitura
+}
